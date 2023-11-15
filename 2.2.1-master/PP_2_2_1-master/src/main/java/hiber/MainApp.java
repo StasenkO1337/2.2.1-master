4aa1a470402ebe6be7d15b51f0b5ad2f780a8ab1
StasenkO1337 <stasenko322@yandex.ru>
2023-11-15 13:48:08 +0500

    Initial commit

---
+++ 2.2.1-master/PP_2_2_1-master/src/main/java/hiber/MainApp.java
@@ -0,0 +1,36 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+      Car car1 = new Car("s1", 1);
+      Car car2 = new Car("s2", 2);
+      Car car3 = new Car("s3", 3);
+      Car car4 = new Car("s4", 4);
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println(user);
+      }
+
+      System.out.println("User with car s4 and series 4 is user with id => " + userService.getUserByCar("s4", 4).getId());
+
+      context.close();
+   }
+}
